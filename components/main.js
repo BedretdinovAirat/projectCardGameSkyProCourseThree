6eba6a7230737b04c4a23dc7da6ba31aaee9ad7f
BedretdinovAirat <bedretdinov-a@mail.ru>
2023-08-17 22:06:38 +0300

    17.08/2023 - не понимаю ТЗ

--- components/main.js
+++ components/main.js
@@ -1,14 +1,20 @@
 const main = document.querySelector(".main");
 
-document.getElementById("easy__game").addEventListener("click", () => {
+const games = {
+  lvl: "",
+};
+window.document.getElementById("easy__game").addEventListener("click", () => {
   alert("ты нажал на 1");
   main.innerHTML = "Ты выбрал легкую игру!";
+  localStorage.setItem("games", JSON.stringify(games));
 });
-document.getElementById("medium__game").addEventListener("click", () => {
+window.document.getElementById("medium__game").addEventListener("click", () => {
   alert("ты нажал на 2");
   main.innerHTML = "Ты выбрал среднюю игру!";
+  localStorage.setItem("games", JSON.stringify(games));
 });
-document.getElementById("hard__game").addEventListener("click", () => {
+window.document.getElementById("hard__game").addEventListener("click", () => {
   alert("ты нажал на 3");
   main.innerHTML = "Ты выбрал сложную игру!";
+  localStorage.setItem("games", JSON.stringify(games));
 });
