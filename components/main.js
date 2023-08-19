962bfc9b1ed5429d45ee8c353b8ffe47d95e1103
BedretdinovAirat <bedretdinov-a@mail.ru>
2023-08-19 22:37:02 +0300

    19.08.2023 - 2 домашка.

--- components/main.js
+++ components/main.js
@@ -1,20 +1,39 @@
 const main = document.querySelector(".main");
 
 const games = {
-  lvl: "",
+  level: "",
 };
 window.document.getElementById("easy__game").addEventListener("click", () => {
   alert("ты нажал на 1");
-  main.innerHTML = "Ты выбрал легкую игру!";
-  localStorage.setItem("games", JSON.stringify(games));
+  // main.innerHTML = "Ты выбрал легкую игру!";
+  // localStorage.setItem("games", JSON.stringify(games));
+  games.level = 1;
 });
 window.document.getElementById("medium__game").addEventListener("click", () => {
   alert("ты нажал на 2");
-  main.innerHTML = "Ты выбрал среднюю игру!";
-  localStorage.setItem("games", JSON.stringify(games));
+  // main.innerHTML = "Ты выбрал среднюю игру!";
+  // localStorage.setItem("games", JSON.stringify(games));
+  games.level = 2;
 });
 window.document.getElementById("hard__game").addEventListener("click", () => {
   alert("ты нажал на 3");
-  main.innerHTML = "Ты выбрал сложную игру!";
-  localStorage.setItem("games", JSON.stringify(games));
+  // main.innerHTML = "Ты выбрал сложную игру!";
+  // localStorage.setItem("games", JSON.stringify(games));
+  games.level = 3;
 });
+window.document
+  .getElementById("button__start")
+  .addEventListener("click", () => {
+    alert("yes");
+    switch (games.level) {
+      case 1:
+        main.innerHTML = "Ты выбрал легкую игру!";
+        break;
+      case 2:
+        main.innerHTML = "Ты выбрал среднюю игру!";
+      case 3:
+        main.innerHTML = "Ты выбрал сложную игру!";
+      default:
+        break;
+    }
+  });
