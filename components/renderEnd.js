644ef58753abe0d7d920329a41f048dd9a040cd8
BedretdinovAirat <bedretdinov-a@mail.ru>
2024-01-28 00:45:26 +0300

    28.01.2024 - логика работает

--- components/renderEnd.js
+++ components/renderEnd.js
@@ -1,5 +1,6 @@
 import { mainCard, reverseCard, counterGame, games } from "./main.js";
 import { time } from "./renderCounter.js";
+import { renderPage } from "./renderPage.js";
 export { endPage };
 const appElement = document.getElementById("app");
 const endPage = () => {
@@ -7,32 +8,40 @@ const endPage = () => {
     <div class="result-game">
       <img
         class="celebration"
-        src="/img/mainImg/celebration.png"
+        src="./img/mainImg/celebration.svg"
         alt="celebration"
       />
       <div class="title-game">Вы выиграли!</div>
       <div class="end_text">Затраченное время:</div>
       <div class="time-game">${time}</div>
-      <button class="restart-button">Играть снова</button>
+      <button id="win-button" class="restart-button">Играть снова</button>
     </div>
   </div>`;
   const loseHTML = `<div class="main__game_end">
     <div class="result-game">
       <img
         class="celebration"
-        src="/img/mainImg/dead.png"
+        src="./img/mainImg/dead.svg"
         alt="dead"
       />
       <div class="title-game">Вы проиграли!</div>
       <div class="end_text">Затраченное время:</div>
       <div class="time-game">${time}</div>
-      <button class="restart-button">Играть снова</button>
+      <button id="lose-button" class="restart-button">Играть снова</button>
     </div>
   </div>`;
   if (counterGame % games.level === 0) {
     appElement.innerHTML = winHTML;
+    const winButton = document.getElementById("win-button");
+    winButton.addEventListener("click", () => {
+      renderPage();
+    });
   }
   if (mainCard !== reverseCard) {
     appElement.innerHTML = loseHTML;
+    const loseButton = document.getElementById("lose-button");
+    loseButton.addEventListener("click", () => {
+      renderPage();
+    });
   }
 };
--- dist/bundle.js
+++ dist/bundle.js
@@ -261,6 +261,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _main_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./main.js */ "./components/main.js");
 /* harmony import */ var _renderCounter_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./renderCounter.js */ "./components/renderCounter.js");
+/* harmony import */ var _renderPage_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./renderPage.js */ "./components/renderPage.js");
+
 
 
 
@@ -270,33 +272,41 @@ const endPage = () => {
     <div class="result-game">
       <img
         class="celebration"
-        src="/img/mainImg/celebration.png"
+        src="./img/mainImg/celebration.svg"
         alt="celebration"
       />
       <div class="title-game">Вы выиграли!</div>
       <div class="end_text">Затраченное время:</div>
       <div class="time-game">${_renderCounter_js__WEBPACK_IMPORTED_MODULE_1__.time}</div>
-      <button class="restart-button">Играть снова</button>
+      <button id="win-button" class="restart-button">Играть снова</button>
     </div>
   </div>`;
   const loseHTML = `<div class="main__game_end">
     <div class="result-game">
       <img
         class="celebration"
-        src="/img/mainImg/dead.png"
+        src="./img/mainImg/dead.svg"
         alt="dead"
       />
       <div class="title-game">Вы проиграли!</div>
       <div class="end_text">Затраченное время:</div>
       <div class="time-game">${_renderCounter_js__WEBPACK_IMPORTED_MODULE_1__.time}</div>
-      <button class="restart-button">Играть снова</button>
+      <button id="lose-button" class="restart-button">Играть снова</button>
     </div>
   </div>`;
   if (_main_js__WEBPACK_IMPORTED_MODULE_0__.counterGame % _main_js__WEBPACK_IMPORTED_MODULE_0__.games.level === 0) {
     appElement.innerHTML = winHTML;
+    const winButton = document.getElementById("win-button");
+    winButton.addEventListener("click", () => {
+      (0,_renderPage_js__WEBPACK_IMPORTED_MODULE_2__.renderPage)();
+    });
   }
   if (_main_js__WEBPACK_IMPORTED_MODULE_0__.mainCard !== _main_js__WEBPACK_IMPORTED_MODULE_0__.reverseCard) {
     appElement.innerHTML = loseHTML;
+    const loseButton = document.getElementById("lose-button");
+    loseButton.addEventListener("click", () => {
+      (0,_renderPage_js__WEBPACK_IMPORTED_MODULE_2__.renderPage)();
+    });
   }
 };
 
